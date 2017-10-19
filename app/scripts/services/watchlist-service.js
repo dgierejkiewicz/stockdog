328f08136330dd7d9ba628fc0e078d1b2f87c9c7
Dariusz <dariusz.gierejkiewicz@gmail.com>
2017-10-19 20:31:57 +0200

    watchlist model

--- app/scripts/services/watchlist-service.js
+++ app/scripts/services/watchlist-service.js
@@ -11,6 +11,54 @@ angular.module('stockdogApp')
   .service('WatchlistService', function WatchlistService() {
     // AngularJS will instantiate a singleton by calling "new" on this function
 
+    // Stocks with helper functions
+    var StockModel = {
+      save: function () {
+        var watchlist = findById(this.listId);
+        watchlist.recalculate();
+        saveModel();
+      }
+    };
+
+    var WatchlistModel = {
+      addStock: function (stock) {
+        var existingStock = _.find(this.stocks, function (s) {
+          return s.company.symbol === stock.company.symbol;
+        });
+        if (existingStock) {
+          existingStock.shares += stock.shares;
+        } else {
+          _.extend(stock, StockModel);
+          this.stock.push(stock);
+        }
+        this.recalculate();
+        saveModel();
+      },
+      removeStock: function (stock) {
+        _.remove(this.stocks, function (s) {
+          return s.company.symbol === stock.company.symbol;
+        });
+        this.recalculate();
+        saveModel();
+      },
+      recalculate: function () {
+        var calcs = _.reduce(this.stocks, function (calcs, stock) {
+          calcs.shares += stock.shares;
+          calcs.marketValue += stock.marketValue;
+          calcs.dayChange += stock.dayChange;
+          return calcs;
+        }, {
+          shares: 0,
+          marketValue: 0,
+          dayChange: 0
+        });
+        this.shares = calcs.shares;
+        this.marketValue = calcs.marketValue;
+        this.dayChange = calcs.dayChange;
+
+      }
+    };
+
     // [1] Helper: Load watchlists from local storage
     var loadModel = function () {
       var model = {
